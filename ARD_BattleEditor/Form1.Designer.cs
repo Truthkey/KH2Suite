7fca75f4849c523a691e257e5291668da13a30fc
Truthkey <verdadmodificada@hotmail.com>
2018-05-17 16:42:51 +0200

    First Commit
    First version of the ARD Battle Editor.

---
+++ ARD_BattleEditor/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ARD_BattleEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadARD = new System.Windows.Forms.Button();
+            this.EventTabControl = new System.Windows.Forms.TabControl();
+            this.MapPreview = new System.Windows.Forms.PictureBox();
+            this.SaveARD = new System.Windows.Forms.Button();
+            this.LoadedFileLabel = new System.Windows.Forms.Label();
+            this.MapScaleTracker = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MapPreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MapScaleTracker)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadARD
+            // 
+            this.LoadARD.Location = new System.Drawing.Point(13, 13);
+            this.LoadARD.Name = "LoadARD";
+            this.LoadARD.Size = new System.Drawing.Size(90, 60);
+            this.LoadARD.TabIndex = 0;
+            this.LoadARD.Text = "Load ARD";
+            this.LoadARD.UseVisualStyleBackColor = true;
+            this.LoadARD.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // EventTabControl
+            // 
+            this.EventTabControl.Location = new System.Drawing.Point(13, 80);
+            this.EventTabControl.Name = "EventTabControl";
+            this.EventTabControl.SelectedIndex = 0;
+            this.EventTabControl.Size = new System.Drawing.Size(1105, 655);
+            this.EventTabControl.TabIndex = 1;
+            // 
+            // MapPreview
+            // 
+            this.MapPreview.Location = new System.Drawing.Point(1124, 80);
+            this.MapPreview.Name = "MapPreview";
+            this.MapPreview.Size = new System.Drawing.Size(512, 512);
+            this.MapPreview.TabIndex = 2;
+            this.MapPreview.TabStop = false;
+            this.MapPreview.Click += new System.EventHandler(this.MapPreview_Click);
+            // 
+            // SaveARD
+            // 
+            this.SaveARD.Enabled = false;
+            this.SaveARD.Location = new System.Drawing.Point(107, 13);
+            this.SaveARD.Name = "SaveARD";
+            this.SaveARD.Size = new System.Drawing.Size(90, 60);
+            this.SaveARD.TabIndex = 5;
+            this.SaveARD.Text = "Save ARD";
+            this.SaveARD.UseVisualStyleBackColor = true;
+            this.SaveARD.Click += new System.EventHandler(this.SaveARD_Click);
+            // 
+            // LoadedFileLabel
+            // 
+            this.LoadedFileLabel.AutoSize = true;
+            this.LoadedFileLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LoadedFileLabel.Location = new System.Drawing.Point(216, 60);
+            this.LoadedFileLabel.Name = "LoadedFileLabel";
+            this.LoadedFileLabel.Size = new System.Drawing.Size(58, 13);
+            this.LoadedFileLabel.TabIndex = 6;
+            this.LoadedFileLabel.Text = "File loaded";
+            this.LoadedFileLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MapScaleTracker
+            // 
+            this.MapScaleTracker.LargeChange = 2;
+            this.MapScaleTracker.Location = new System.Drawing.Point(1125, 598);
+            this.MapScaleTracker.Maximum = 8;
+            this.MapScaleTracker.Name = "MapScaleTracker";
+            this.MapScaleTracker.Size = new System.Drawing.Size(510, 45);
+            this.MapScaleTracker.TabIndex = 7;
+            this.MapScaleTracker.Scroll += new System.EventHandler(this.MapScaleTracker_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1349, 646);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "MAP SCALE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1648, 747);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MapScaleTracker);
+            this.Controls.Add(this.LoadedFileLabel);
+            this.Controls.Add(this.SaveARD);
+            this.Controls.Add(this.MapPreview);
+            this.Controls.Add(this.EventTabControl);
+            this.Controls.Add(this.LoadARD);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "ARD -Battle Editor-";
+            ((System.ComponentModel.ISupportInitialize)(this.MapPreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MapScaleTracker)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadARD;
+        private System.Windows.Forms.PictureBox MapPreview;
+        private System.Windows.Forms.TabControl EventTabControl;
+        private System.Windows.Forms.Button SaveARD;
+        private System.Windows.Forms.Label LoadedFileLabel;
+        private System.Windows.Forms.TrackBar MapScaleTracker;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ARD_BattleEditor/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace ARD_BattleEditor
+{
+    public partial class Form1 : Form
+    {
+        List<UInt32> FilePositions;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Image GetImageByPreview(string filename)
+        {
+            string CleanFileName = Path.GetFileName(filename);
+
+            LoadedFileLabel.Text = CleanFileName;
+
+            switch (CleanFileName)
+            {
+                case "al00.ard":
+                    return (Image)new Bitmap(Properties.Resources.al00);
+                case "al01.ard":
+                    return (Image)new Bitmap(Properties.Resources.al01);
+                case "al02.ard":
+                    return (Image)new Bitmap(Properties.Resources.al00);
+                case "al03.ard":
+                    return (Image)new Bitmap(Properties.Resources.al00);
+                case "al04.ard":
+                    return (Image)new Bitmap(Properties.Resources.al00);
+                case "al05.ard":
+                    return (Image)new Bitmap(Properties.Resources.al00);
+            }
+
+            return null;
+        }
+
+        private string[] GetAllStringsFromResource()
+        {
+            return Properties.Resources.UCMText.Split(Environment.NewLine.ToCharArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog();
+
+            if(diag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //Clear tabs.
+                EventTabControl.TabPages.Clear();
+
+                FileStream f = new FileStream(diag.FileName, FileMode.Open);
+                BinaryReader r = new BinaryReader(f);
+
+                string[] UCMStrings = GetAllStringsFromResource();
+                FilePositions = new List<UInt32>();
+                FilePositions.Clear();
+
+                if (r.ReadUInt32() == 0x01524142)
+                {
+                    UInt32 FileCount = r.ReadUInt32();
+
+                    for (UInt32 i = 0; i < FileCount; i++)
+                    {
+                        // Get file format ID.
+                        r.BaseStream.Seek(0x10 + i * 0x10, SeekOrigin.Begin);
+                        UInt32 FileFormatID = r.ReadUInt32();
+
+                        r.BaseStream.Seek(0x18 + i * 0x10, SeekOrigin.Begin);
+                        UInt32 FilePosition = r.ReadUInt32();
+
+                        MapPreview.Image = GetImageByPreview(diag.FileName);
+
+                        if (FileFormatID == 0xC)
+                        {
+                            FilePositions.Add(i);
+
+                            r.BaseStream.Seek(FilePosition + 0xC, SeekOrigin.Begin);
+                            UInt16 NumberOfEntities = r.ReadUInt16();
+
+                            r.BaseStream.Seek(0x14 + i * 0x10, SeekOrigin.Begin);
+                            TabPage tab = new TabPage(new string(r.ReadChars(4)));
+
+                            // Read appear data and add combo boxes to each tab.
+                            for(int j = 0; j < NumberOfEntities; j++)
+                            {
+                                r.BaseStream.Seek(FilePosition + 0x34 + 0x40*j, SeekOrigin.Begin);
+                                UInt32 UCM = r.ReadUInt32();
+                                
+                                ComboBox nBox = new ComboBox();
+                                nBox.Items.AddRange(UCMStrings);
+                                nBox.Width = 250;
+                                nBox.SelectedIndex = (int)UCM;
+                                nBox.Location = new Point(0, j * 50);
+
+                                tab.Controls.Add(nBox);
+                            }
+
+                            EventTabControl.TabPages.Add(tab);
+                        }
+                    }
+
+                    // Enable the save button if there is any compatible file.
+                    SaveARD.Enabled = FilePositions.Count > 0;
+                }
+                else
+                {
+                    MessageBox.Show("Not a BAR file.");
+                }
+            }
+        }
+
+        private void MapPreview_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveARD_Click(object sender, EventArgs e)
+        {
+            if(FilePositions.Count > 0)
+            {
+                for(UInt32 i = 0; i < EventTabControl.Controls.Count; i++)
+                {
+                    TabPage tabPage = (TabPage)EventTabControl.GetControl((int)i);
+
+                    foreach (Control control in tabPage.Controls)
+                    {
+                        MessageBox.Show(control.GetType().ToString());
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is no ARD loaded.");
+            }
+        }
+
+        private void MapScaleTracker_Scroll(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
